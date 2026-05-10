30ad49807b08e0ce654bd34a604f47c9f951f5cb
Bielzino <123326154+Bielzino@users.noreply.github.com>
2026-05-10 12:49:39 -0300

    update js test

--- login/js/login.js
+++ login/js/login.js
@@ -90,12 +90,7 @@ function setName(nome){
     nomeReg.textContent = nome;
 }
 
-window.addEventListener("load", () => {
-    // Verificamos se o objeto 'cef' existe para evitar erros no console
-    if (typeof cef !== 'undefined') {
-        cef.on("UI:SetName", function (nome) {
-            console.log("Nome recebido do servidor:", nome);
-            setName(nome);
-        });
-    }
+cef.on("UI:SetName", function (nome) {
+    console.log("Nome recebido do servidor:", nome);
+    setName(nome);
 });
